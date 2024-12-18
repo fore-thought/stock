d84f0cbb7886f1178b5d5f813414eb941cde88e7
Murphy <murphy-work@foxmail.com>
2024-12-18 18:18:46 +0800

    sensitive configuration, build and deployment process

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,32 @@
+/*
+ * Copyright (c) [2024] [Murphy]
+ * [Stock] is licensed under Mulan PubL v2.
+ * You can use this software according to the terms and conditions of the Mulan PubL v2.
+ * You may obtain a copy of Mulan PubL v2 at:
+ *          http://license.coscl.org.cn/MulanPubL-2.0
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PubL v2 for more details.
+ */
+
 plugins {
     java
     id("io.freefair.lombok") version "8.11"
+    `maven-publish`
 }
 
 group = "tech.forethought"
 version = "1.1-SNAPSHOT"
 
 repositories {
+    maven {
+        url = uri(System.getenv("MAVEN_URL"))
+        credentials {
+            username = System.getenv("MAVEN_USERNAME")
+            password = System.getenv("MAVEN_PASSWORD")
+        }
+    }
     mavenCentral()
 }
 
@@ -29,6 +49,17 @@ tasks.withType<JavaCompile> {
     options.compilerArgs.add("-parameters")
 }
 
+tasks.withType<ProcessResources> {
+    filesMatching("app.yml") {
+        expand(
+            "JDBC_URL" to System.getenv("JDBC_URL"),
+            "DB_USERNAME" to System.getenv("DB_USERNAME"),
+            "DB_PASSWORD" to System.getenv("DB_PASSWORD"),
+            "MAIRUI_LICENCE" to System.getenv("MAIRUI_LICENCE")
+        )
+    }
+}
+
 tasks.jar {
     archiveBaseName = "stock"
     manifest.attributes["Main-Class"] = "tech.forethought.stock.Main"
@@ -38,4 +69,24 @@ tasks.jar {
         if (it.isDirectory) it else zipTree(it)
     })
     from(sourceSets.main.get().output)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "tech.forethought"
+            artifactId = "stock"
+            version = "1.1-SNAPSHOT"
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            url = uri(System.getenv("MAVEN_URL"))
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
 }
